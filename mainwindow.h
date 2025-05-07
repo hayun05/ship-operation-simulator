a608bda6fc30e6dcc240c8a3dcd6a38221d9472f
김하연 <hayun080805@gmail.com>
2025-05-07 15:38:53 +0900

    Update and rename MainWindow to mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,14 @@
+#ifndef DISPLAYPAGE_H
+#define DISPLAYPAGE_H
+
+#include <QWidget>
+
+class DisplayPage : public QWidget
+{
+    Q_OBJECT
+    
+public:
+    explicit DisplayPage(QWidget *parent = nullptr);
+};
+
+#endif // DISPLAYPAGE_H
