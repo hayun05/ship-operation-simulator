fbec63048b6efd11e9453a96f0d8c4f0dc5375ea
김하연 <hayun080805@gmail.com>
2025-06-18 15:59:49 +0900

    Update settingpage.h

--- settingpage.h
+++ settingpage.h
@@ -11,6 +11,7 @@
 #include <QSpinBox>
 #include <QCheckBox>
 #include <QMessageBox>
+#include <QSlider>
 
 class SettingPage : public QWidget
 {
@@ -29,21 +30,40 @@ private:
 
     QHBoxLayout* BLayout1;
     QHBoxLayout* BLayout2;
+    QHBoxLayout* BLayout3;
+
     QHBoxLayout* PLayout1;
+    QHBoxLayout* PLayout2;
 
     QLabel* Blb1;
     QLabel* Blb2;
+    QLabel* Blb3;
 
     QLabel* Plb1;
 
     QSpinBox* boatSize;
     QComboBox* boatSelcet;
+    QSlider* boatSpeed;
+    QSlider* wind;
 
     QCheckBox* rain;
 
 
+
 signals:
-    void checkedRain(bool checked);
+    void onRain(bool check);
+    void onSpeedChanged(int speed);
+    void onSelectBoat(int index);
+    void onSizeChecked(int size);
+    void onWind(int wind);
+
+private slots:
+    void checkedRain(bool check);
+    void speedChanged(int speed);
+    void selectBoat(int index);
+    void sizeChecked(int size);
+    void windChecked(int wind);
+
 };
 
 #endif // SETTINGPAGE_H
