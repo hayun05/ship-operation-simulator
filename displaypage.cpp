24f5eb349a3dfd0e42325c0ff4b9e03dc1440430
김하연 <hayun080805@gmail.com>
2025-06-18 15:58:17 +0900

    Update displaypage.cpp

--- displaypage.cpp
+++ displaypage.cpp
@@ -6,9 +6,17 @@ DisplayPage::DisplayPage(QWidget *parent)
     : QWidget(parent)
 {
     QVBoxLayout *layout = new QVBoxLayout(this);
+
     QLabel *label = new QLabel("Display Page", this);
+    display3d = new Display3D(this); // 3D 위젯 생성
+
     layout->addWidget(label);
+    layout->addWidget(display3d);  // 3D 위젯 추가
     layout->addStretch();
 
     setLayout(layout);
+}
+
+Display3D* DisplayPage::get3DWidget() {
+    return display3d;
 }
