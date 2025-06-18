ecc5681e87ec2ea3ae8eca0aa31d5c290289181d
김하연 <hayun080805@gmail.com>
2025-06-18 15:56:07 +0900

    Update controller.h

--- controller.h
+++ controller.h
@@ -7,5 +7,9 @@ public:
     explicit Controller(QObject *parent = nullptr);
 
 public slots:
-    void rainChecked(bool checked);  // 슬롯 정의
+    void onRain(bool checked);  // 슬롯 정의
+    void onSpeedChanged(int spped);
+    void onSelectBoat(int index);
+    void onSizeChecked(int size);
+    void onWind(int wind);
 };
