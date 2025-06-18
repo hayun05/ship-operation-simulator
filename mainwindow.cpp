837c89893eaf29c007731f184d463e557622552d
김하연 <hayun080805@gmail.com>
2025-06-18 15:59:08 +0900

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,69 +1,87 @@
-#ifndef SETTINGPAGE_H
-#define SETTINGPAGE_H
+#include "mainwindow.h"
+#include "settingpage.h"
+#include "displaypage.h"
+#include "controller.h"
+#include "display3d.h"
 
-#include <QWidget>
-#include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QLabel>
-#include <QGroupBox>
-#include <QComboBox>
-#include <QSpacerItem>
-#include <QSpinBox>
-#include <QCheckBox>
-#include <QMessageBox>
-#include <QSlider>
+#include <QMenuBar>
+#include <QMenu>
+#include <QAction>
+#include <QScreen>
+#include <QGuiApplication>
+#include <QObject>
 
-class SettingPage : public QWidget
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
 {
-    Q_OBJECT
+    // 메뉴 생성
+    QMenu *menu = menuBar()->addMenu("Menu");
+    QAction *settingAction = new QAction("Setting", this);
+    QAction *displayAction = new QAction("Display", this);
 
-public:
-    explicit SettingPage(QWidget *parent = nullptr);
+    menu->addAction(settingAction);
+    menu->addAction(displayAction);
+
+    // 페이지 생성
+    SettingPage *settingPage = new SettingPage(this);
+    DisplayPage *displayPage = new DisplayPage(this);
+    Controller *controller = new Controller();
+    Display3D *display3d = new Display3D();
+
+    // QStackedWidget 설정
+    stackedWidget = new QStackedWidget(this);
+    stackedWidget->addWidget(settingPage);   // index 0
+    stackedWidget->addWidget(displayPage);   // index 1
+    stackedWidget->setCurrentIndex(1);       // ⭐ 기본 화면을 Display로 설정
+    setCentralWidget(stackedWidget);
+
+    setWindowTitle("Multi-Page MainWindow");
+    resize(600, 400);
+
+    // 화면 해상도 기반 크기 계산
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QSize screenSize = screen->availableGeometry().size();
+
+    // 화면의 80%를 기준으로 하되, 10:8 비율 유지
+    int width = screenSize.width() * 0.8;
+    int height = screenSize.height() * 0.8;  // 10:8 비율
+
+    setFixedSize(width, height);  // 창 크기 고정
+    // 메뉴 동작 연결
+    connect(settingAction, &QAction::triggered, [=]() {
+        stackedWidget->setCurrentIndex(0);
+    });
+
+    connect(displayAction, &QAction::triggered, [=]() {
+        stackedWidget->setCurrentIndex(1);
+    });
 
 
-private:
-    QGroupBox *boatGroup;
-    QGroupBox *prefGroup;
+    //controller - settingWidget 연결
 
-    QVBoxLayout *boatLayout;
-    QVBoxLayout *prefLayout;
+    QObject::connect(settingPage, &SettingPage::onRain,
+                     controller, &Controller::onRain);
 
-    QHBoxLayout* BLayout1;
-    QHBoxLayout* BLayout2;
-    QHBoxLayout* BLayout3;
+    QObject::connect(settingPage, &SettingPage::onSpeedChanged,
+                     controller, &Controller::onSpeedChanged);
 
-    QHBoxLayout* PLayout1;
-    QHBoxLayout* PLayout2;
+    QObject::connect(settingPage, &SettingPage::onSelectBoat,
+                     controller, &Controller::onSelectBoat);
 
-    QLabel* Blb1;
-    QLabel* Blb2;
-    QLabel* Blb3;
+    QObject::connect(settingPage, &SettingPage::onSizeChecked,
+                     controller, &Controller::onSizeChecked);
 
-    QLabel* Plb1;
-
-    QSpinBox* boatSize;
-    QComboBox* boatSelcet;
-    QSlider* boatSpeed;
-    QSlider* wind;
-
-    QCheckBox* rain;
+    QObject::connect(settingPage, &SettingPage::onWind,
+                     controller, &Controller::onWind);
 
 
+    //controller-display3d 연결
 
-signals:
-    void onRain(bool check);
-    void onSpeedChanged(int speed);
-    void onSelectBoat(int index);
-    void onSizeChecked(int size);
-    void onWind(int wind);
 
-private slots:
-    void checkedRain(bool check);
-    void speedChanged(int speed);
-    void selectBoat(int index);
-    void sizeChecked(int size);
-    void windChecked(int wind);
+    // 연결
+    QObject::connect(controller, &Controller::onSpeedChanged, display3d, &Display3D::updateSpeed);
+    QObject::connect(controller, &Controller::onRain, display3d, &Display3D::updateRain);
+    QObject::connect(controller, &Controller::onSelectBoat, display3d, &Display3D::updateBoat);
 
-};
-
-#endif // SETTINGPAGE_H
+}
