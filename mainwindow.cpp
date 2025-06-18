0498dbdf4a906bc498d84da0d4075254297220f8
김하연 <hayun080805@gmail.com>
2025-06-18 15:58:46 +0900

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,59 +1,69 @@
-#include "mainwindow.h"
-#include "settingpage.h"
-#include "displaypage.h"
-#include "controller.h"
+#ifndef SETTINGPAGE_H
+#define SETTINGPAGE_H
 
-#include <QMenuBar>
-#include <QMenu>
-#include <QAction>
-#include <QScreen>
-#include <QGuiApplication>
-#include <QObject>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QGroupBox>
+#include <QComboBox>
+#include <QSpacerItem>
+#include <QSpinBox>
+#include <QCheckBox>
+#include <QMessageBox>
+#include <QSlider>
 
-
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+class SettingPage : public QWidget
 {
-    // 메뉴 생성
-    QMenu *menu = menuBar()->addMenu("Menu");
-    QAction *settingAction = new QAction("Setting", this);
-    QAction *displayAction = new QAction("Display", this);
+    Q_OBJECT
 
-    menu->addAction(settingAction);
-    menu->addAction(displayAction);
+public:
+    explicit SettingPage(QWidget *parent = nullptr);
 
-    // 페이지 생성
-    SettingPage *settingPage = new SettingPage(this);
-    DisplayPage *displayPage = new DisplayPage(this);
-    Controller *controller = new Controller();
 
-    // QStackedWidget 설정
-    stackedWidget = new QStackedWidget(this);
-    stackedWidget->addWidget(settingPage);   // index 0
-    stackedWidget->addWidget(displayPage);   // index 1
-    stackedWidget->setCurrentIndex(1);       // ⭐ 기본 화면을 Display로 설정
-    setCentralWidget(stackedWidget);
+private:
+    QGroupBox *boatGroup;
+    QGroupBox *prefGroup;
 
-    setWindowTitle("Multi-Page MainWindow");
-    resize(600, 400);
+    QVBoxLayout *boatLayout;
+    QVBoxLayout *prefLayout;
 
-    // 화면 해상도 기반 크기 계산
-    QScreen *screen = QGuiApplication::primaryScreen();
-    QSize screenSize = screen->availableGeometry().size();
+    QHBoxLayout* BLayout1;
+    QHBoxLayout* BLayout2;
+    QHBoxLayout* BLayout3;
 
-    // 화면의 80%를 기준으로 하되, 10:8 비율 유지
-    int width = screenSize.width() * 0.8;
-    int height = screenSize.height() * 0.8;  // 10:8 비율
+    QHBoxLayout* PLayout1;
+    QHBoxLayout* PLayout2;
 
-    setFixedSize(width, height);  // 창 크기 고정
-    // 메뉴 동작 연결
-    connect(settingAction, &QAction::triggered, [=]() {
-        stackedWidget->setCurrentIndex(0);
-    });
+    QLabel* Blb1;
+    QLabel* Blb2;
+    QLabel* Blb3;
 
-    connect(displayAction, &QAction::triggered, [=]() {
-        stackedWidget->setCurrentIndex(1);
-    });
+    QLabel* Plb1;
 
-    QObject::connect(settingPage, &SettingPage::checkedRain, controller, &Controller::rainChecked);
-}
+    QSpinBox* boatSize;
+    QComboBox* boatSelcet;
+    QSlider* boatSpeed;
+    QSlider* wind;
+
+    QCheckBox* rain;
+
+
+
+signals:
+    void onRain(bool check);
+    void onSpeedChanged(int speed);
+    void onSelectBoat(int index);
+    void onSizeChecked(int size);
+    void onWind(int wind);
+
+private slots:
+    void checkedRain(bool check);
+    void speedChanged(int speed);
+    void selectBoat(int index);
+    void sizeChecked(int size);
+    void windChecked(int wind);
+
+};
+
+#endif // SETTINGPAGE_H
