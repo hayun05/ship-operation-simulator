b4ec97710b2ec449d817284c037d82703ccace9c
김하연 <hayun080805@gmail.com>
2025-06-18 15:55:54 +0900

    Update controller.cpp

--- controller.cpp
+++ controller.cpp
@@ -1,11 +1,32 @@
 #include "controller.h"
+#include "settingpage.h"
+
 #include <QDebug>
 
 Controller::Controller(QObject *parent) : QObject(parent) {}
 
-void Controller::rainChecked(bool checked) {
+
+void Controller::onSpeedChanged(int speed)
+{
+    qDebug() << "Speed:"<< speed << '\n';
+}
+
+void Controller::onRain(bool checked) {
+
     if (checked)
-        qDebug() << "Checkbox is checked!";
+        qDebug() << "Checkbox is checked!" << '\n';
     else
-        qDebug() << "Checkbox is unchecked!";
+        qDebug() << "Checkbox is unchecked!" << '\n';
+}
+
+void Controller::onSelectBoat(int index){
+    qDebug() << "Select Number:" << index << '\n';
+}
+
+void Controller::onSizeChecked(int size){
+    qDebug() << "boat Size : " <<size << '\n';
+}
+
+void Controller::onWind(int wind){
+    qDebug() << "WIND" << wind << '\n';
 }
