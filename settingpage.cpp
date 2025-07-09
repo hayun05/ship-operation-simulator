36b39b54e33caf6786d8d6404b65090c4f0cc3c8
김하연 <hayun080805@gmail.com>
2025-07-09 15:47:53 +0900

    Update settingpage.cpp

--- settingpage.cpp
+++ settingpage.cpp
@@ -1,64 +1,102 @@
-#ifndef SETTINGPAGE_H
-#define SETTINGPAGE_H
+#include "settingpage.h"
 
-#include <QWidget>
-#include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QLabel>
-#include <QGroupBox>
-#include <QComboBox>
-#include <QSpacerItem>
-#include <QSpinBox>
-#include <QCheckBox>
-#include <QSlider>
-
-class SettingPage : public QWidget
+SettingPage::SettingPage(QWidget *parent)
+    : QWidget(parent)
 {
-    Q_OBJECT
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
 
-public:
-    explicit SettingPage(QWidget *parent = nullptr);
+    // === Boat Settings ===
+    boatGroup = new QGroupBox("Boat Setting", this);
+    boatLayout = new QVBoxLayout(boatGroup);
 
-private:
-    QGroupBox *boatGroup;
-    QGroupBox *prefGroup;
+    // Boat Type
+    BLayout1 = new QHBoxLayout();
+    Blb1 = new QLabel("배 종류 선택", this);
+    boatSelect = new QComboBox(this);
+    boatSelect->addItems({"고속정", "어선", "잠수함"});
+    BLayout1->addWidget(Blb1);
+    BLayout1->addWidget(boatSelect);
+    BLayout1->addStretch();
+    boatLayout->addLayout(BLayout1);
 
-    QVBoxLayout *boatLayout;
-    QVBoxLayout *prefLayout;
+    // Boat Size
+    BLayout2 = new QHBoxLayout();
+    Blb2 = new QLabel("배 크기 조정", this);
+    boatSize = new QSpinBox(this);
+    BLayout2->addWidget(Blb2);
+    BLayout2->addWidget(boatSize);
+    BLayout2->addStretch();
+    boatLayout->addLayout(BLayout2);
 
-    QHBoxLayout* BLayout1;
-    QHBoxLayout* BLayout2;
-    QHBoxLayout* BLayout3;
+    // Boat Speed
+    BLayout3 = new QHBoxLayout();
+    Blb3 = new QLabel("배 속도 조정", this);
+    boatSpeed = new QSlider(Qt::Horizontal, this);
+    boatSpeed->setRange(1, 100);
+    boatSpeed->setValue(20);
+    BLayout3->addWidget(Blb3);
+    BLayout3->addWidget(boatSpeed);
+    BLayout3->addStretch();
+    boatLayout->addLayout(BLayout3);
 
-    QHBoxLayout* PLayout1;
-    QHBoxLayout* PLayout2;
-
-    QLabel* Blb1;
-    QLabel* Blb2;
-    QLabel* Blb3;
-    QLabel* Plb1;
-
-    QSpinBox* boatSize;
-    QComboBox* boatSelect;
-    QSlider* boatSpeed;
-    QSlider* wind;
-    QCheckBox* rain;
-
-signals:
-    void rainToggled(bool);
-    void speedValueChanged(int);
-    void boatTypeChanged(int);
-    void sizeValueChanged(int);
-    void windValueChanged(int);
-
-private slots:
-    void handleRain(bool checked);
-    void handleSpeed(int speed);
-    void handleBoatType(int index);
-    void handleSize(int size);
-    void handleWind(int value);
-
-};
+    boatGroup->setLayout(boatLayout);
 
 
-#endif // SETTINGPAGE_H
+    // === Preferences ===
+    prefGroup = new QGroupBox("Preferences", this);
+    prefLayout = new QVBoxLayout(prefGroup);
+
+    // Rain Checkbox
+    PLayout1 = new QHBoxLayout();
+    rain = new QCheckBox("비 켜기", this);
+    PLayout1->addWidget(rain);
+    PLayout1->addStretch();
+    prefLayout->addLayout(PLayout1);
+
+    // Wind Slider
+    PLayout2 = new QHBoxLayout();
+    Plb1 = new QLabel("바람 세기", this);
+    wind = new QSlider(Qt::Horizontal, this);
+    wind->setRange(0, 100);
+    wind->setValue(0);
+    PLayout2->addWidget(Plb1);
+    PLayout2->addWidget(wind);
+    PLayout2->addStretch();
+    prefLayout->addLayout(PLayout2);
+
+    prefGroup->setLayout(prefLayout);
+
+    // === Connections ===
+    connect(rain, &QCheckBox::toggled, this, &SettingPage::handleRain);
+    connect(boatSpeed, &QSlider::valueChanged, this, &SettingPage::handleSpeed);
+    connect(boatSelect, &QComboBox::currentIndexChanged, this, &SettingPage::handleBoatType);
+    connect(boatSize, QOverload<int>::of(&QSpinBox::valueChanged), this, &SettingPage::handleSize);
+    connect(wind, &QSlider::valueChanged, this, &SettingPage::handleWind);
+
+    // Add to main layout
+    mainLayout->addWidget(boatGroup);
+    mainLayout->addWidget(prefGroup);
+    setLayout(mainLayout);
+}
+
+
+// === 슬롯 구현 ===
+void SettingPage::handleRain(bool checked) {
+    emit rainToggled(checked);
+}
+
+void SettingPage::handleSpeed(int speed) {
+    emit speedValueChanged(speed);
+}
+
+void SettingPage::handleBoatType(int index) {
+    emit boatTypeChanged(index);
+}
+
+void SettingPage::handleSize(int size) {
+    emit sizeValueChanged(size);
+}
+
+void SettingPage::handleWind(int value) {
+    emit windValueChanged(value);
+}
