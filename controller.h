d54d5764afdab41db71767cdcc055963eaf9e544
김하연 <hayun080805@gmail.com>
2025-07-09 15:45:36 +0900

    Update controller.h

--- controller.h
+++ controller.h
@@ -1,3 +1,6 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
 #include <QObject>
 
 class Controller : public QObject {
@@ -8,8 +11,10 @@ public:
 
 public slots:
     void onRain(bool checked);  // 슬롯 정의
-    void onSpeedChanged(int spped);
+    void onSpeedChanged(int speed);  // 오타 수정
     void onSelectBoat(int index);
     void onSizeChecked(int size);
     void onWind(int wind);
 };
+
+#endif // CONTROLLER_H
