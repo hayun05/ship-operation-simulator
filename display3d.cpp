dedc0b13685e99f4f81079db3cee67d724e3bf3a
김하연 <hayun080805@gmail.com>
2025-07-09 14:37:22 +0900

    Update display3d.cpp

--- display3d.cpp
+++ display3d.cpp
@@ -1,35 +1,80 @@
 #include "display3d.h"
-#include <QDebug>
+#include <Qt3DRender/QCamera>
 
-Display3D::Display3D(QWidget *parent) : QOpenGLWidget(parent) {}
+Display3D::Display3D(QWidget *parent) : QWidget(parent) {
+    view = new Qt3DExtras::Qt3DWindow();
+    container = QWidget::createWindowContainer(view);
+    container->setMinimumSize(400, 300);
+    container->setFocusPolicy(Qt::StrongFocus);
 
-void Display3D::initializeGL() {
-    initializeOpenGLFunctions();
-    glClearColor(0, 0.3, 0.6, 1); // 바다색
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    layout->addWidget(container);
+    setLayout(layout);
+
+    rootEntity = new Qt3DCore::QEntity();
+    view->setRootEntity(rootEntity);
+
+    // Camera
+    Qt3DRender::QCamera *camera = view->camera();
+    camera->lens()->setPerspectiveProjection(45.0f, 4.0f / 3.0f, 0.1f, 1000.0f);
+    camera->setPosition(QVector3D(0, 5, 20));
+    camera->setViewCenter(QVector3D(0, 0, 0));
+
+    // Camera control
+    auto *camController = new Qt3DExtras::QOrbitCameraController(rootEntity);
+    camController->setCamera(camera);
+
+    createScene();
 }
 
-void Display3D::resizeGL(int w, int h) {
-    glViewport(0, 0, w, h);
+void Display3D::createScene() {
+    // 기본 박스 배
+    boatEntity = new Qt3DCore::QEntity(rootEntity);
+
+    auto *mesh = new Qt3DExtras::QCuboidMesh();
+    auto *material = new Qt3DExtras::QPhongMaterial();
+    material->setDiffuse(Qt::red);
+
+    boatEntity->addComponent(mesh);
+    boatEntity->addComponent(material);
 }
 
-void Display3D::paintGL() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void Display3D::createRain() {
+    for (int i = 0; i < 100; ++i) {
+        RainDrop *drop = new RainDrop(rootEntity);
+        rainDrops.append(drop);
+    }
 
-    // 여기에 배, 파도, 비 등 OpenGL로 렌더링
-    // 예: 속도에 따라 움직이는 배
+    rainTimer = new QTimer(this);
+    connect(rainTimer, &QTimer::timeout, this, &Display3D::updateRainfall);
+    rainTimer->start(30);
+}
+
+void Display3D::updateRainfall() {
+    for (RainDrop *drop : rainDrops) {
+        drop->fall();
+    }
 }
 
 void Display3D::updateSpeed(int s) {
     speed = s;
-    update(); // repaint
+    // 추후 이동 처리 가능
 }
 
 void Display3D::updateRain(bool r) {
     isRaining = r;
-    update();
+    if (isRaining) {
+        if (rainDrops.isEmpty())
+            createRain();
+        rainTimer->start();
+    } else {
+        rainTimer->stop();
+        for (RainDrop *drop : rainDrops)
+            drop->setEnabled(false);
+    }
 }
 
 void Display3D::updateBoat(int index) {
     boatType = index;
-    update();
+    // 추후 보트 변경
 }
